a2d6765678b14550b89ae5f164df370a983a4434
Chloe Steffens <caforonda103@yahoo.com>
2020-06-18 04:22:34 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
+  belongs_to :actors
+  belongs_to :shows
   
+  def say_that_thing_you_say
+    "#{self.name} always says: #{catchphrase}"
+  end
 end
---
+++ db/migrate/006_add_character_catchphrase.rb
@@ -0,0 +1,8 @@
+class AddCharacterCatchphrase < ActiveRecord::Migration[5.2]
+  
+  def change
+    add_column :characters, :catchphrase, :string
+    
+  end
+  
+end
