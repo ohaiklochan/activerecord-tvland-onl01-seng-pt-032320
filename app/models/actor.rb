062fe0e7c655aa524b9af2fac574211458322e24
Chloe Steffens <caforonda103@yahoo.com>
2020-06-18 04:37:48 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,9 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    characters.map{|character| "#{character.name} - #{character.show.name}"}
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end
   
 end
